026b6ff1b74f2fb7060a6b4603deca52980bfab8
Hana-fubuki <jgcoheno@gmail.com>
2026-08-12 18:13:20 -0400

    initial maui project commit

---
+++ CurrencyExchangeRates/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrencyExchangeRates;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CurrencyExchangeRates/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CurrencyExchangeRates;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
